9210dc47920481f62377ac8ed49c1b143d63be6d
Jason Wang <xoutdragon@gmail.com>
2024-02-14 19:24:52 -0500

    Settings Modal Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import { ThemeProvider } from '@/components/providers/theme-providers';
-import { ConvexClientProvider } from '@/components/providers/convex-providers';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ConvexClientProvider } from '@/components/providers/convex-provider';
+import { ModalProvider } from '@/components/providers/modal-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -44,6 +45,7 @@ export default function RootLayout({
 						storageKey='jotion-theme'
 					>
 						<Toaster position='bottom-center' />
+						<ModalProvider />
 						{children}
 					</ThemeProvider>
 				</ConvexClientProvider>
